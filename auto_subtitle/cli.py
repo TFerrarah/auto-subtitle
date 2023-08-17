af4b59f2eb0469192f182b28c1756564b54d4f28
T_Ferrarah <forkiofficialtech@gmail.com>
2023-08-17 16:43:25 +0200

    Update cli.py

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -63,8 +63,9 @@ def main():
         audio = video.audio
 
         ffmpeg.concat(
-            video.filter('subtitles', srt_path, fontsdir="/content/fonts", force_style="Alignment=10,OutlineColour=&H40000000,BorderStyle=3,FontName=Pusab,FontSize=12"), audio, v=1, a=1
+            video.filter('subtitles', srt_path, fontsdir="/content/fonts", force_style="Alignment=10,OutlineColour=&H000000,BorderStyle=3,FontName=Pusab,FontSize=12,BackColour=&H00000000,PrimaryColour=&H00FF0000"), audio, v=1, a=1
         ).output(out_path).run(quiet=True, overwrite_output=True)
+
 
         print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
 
